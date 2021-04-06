01540567610e03abef17d0e1f084b46b25586f9d
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-06 19:24:45 +0100

    added event listener for username

--- form-validator/scripts.js
+++ form-validator/scripts.js
@@ -6,3 +6,16 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+
+
+// Event listeners
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if(username.value === '') {
+       showError(username, 'Username is required') 
+    } else {
+        showSuccess(username);
+    }
+});
