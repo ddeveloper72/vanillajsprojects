84acf438cac2867e68fe04bf0992580d27274c76
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-06 19:10:40 +0100

    add dom elements

--- form-validator/scripts.js
+++ form-validator/scripts.js
@@ -1,1 +1,8 @@
 
+// collect all the dom elements
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
