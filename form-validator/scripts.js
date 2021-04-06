f6b97f610b4bf4ee08a91d2236c6ee08ed75736a
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-06 21:26:47 +0100

    updated email validation function

--- form-validator/scripts.js
+++ form-validator/scripts.js
@@ -22,9 +22,14 @@ function showError(input, message) {
 }
 
 // check if email is an email address
-function validateEmail(email) {
+function validateEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())) {
+        showSuccess(input);
+
+    } else {
+        showError(input, 'Email is not valid')
+    }
 }
 
 
@@ -72,4 +77,5 @@ form.addEventListener('submit', function (e) {
     checkRequired([username, email, password, password2]);
     checkLength(username, 3, 15);
     checkLength(password, 6, 25);
+    validateEmail(email);
 });
