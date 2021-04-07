3e144d8ce5f876103b3771af60ba062412437906
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-07 11:10:13 +0100

    collect DOM elements & ids

--- movie-seat-booking/scripts.js
+++ movie-seat-booking/scripts.js
@@ -1,1 +1,11 @@
-ECHO is on.
+// collect the DOM elements
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('row .seat:not(.occupied)'); // use css class syntax
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+
+const ticketPrice = +movieSelect.value; // set type from string to number
+
+console.log(typeof ticketPrice);
