b25f9ef2fbae6ea0e8a652abc7d156b84f79783d
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-08 10:08:48 +0100

    liust  event listeners

--- custom-video-player/scripts.js
+++ custom-video-player/scripts.js
@@ -4,3 +4,10 @@ const play = document.getElementById('play')
 const stop = document.getElementById('stop')
 const progress = document.getElementById('progress')
 const timestamp = document.getElementById('timestamp')
+
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('pause', updatePauseIcon);
+video.addEventListener('timeupdate', updateProgress);
