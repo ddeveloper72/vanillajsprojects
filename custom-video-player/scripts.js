03cea9e51d4dbff6307592666e2e7005fcc8478d
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-08 10:04:31 +0100

    collect DOM element constants

--- custom-video-player/scripts.js
+++ custom-video-player/scripts.js
@@ -1,1 +1,6 @@
-ECHO is on.
+// collect DOM elements
+const video = document.getElementById('video')
+const play = document.getElementById('play')
+const stop = document.getElementById('stop')
+const progress = document.getElementById('progress')
+const timestamp = document.getElementById('timestamp')
