cfe43f512067ea9b3e57db05cbb43a64e7a766a6
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-09 20:41:16 +0100

    added DOM elements & event listeners

--- exchange-rate-calculator/scripts.js
+++ exchange-rate-calculator/scripts.js
@@ -1,1 +1,26 @@
-ECHO is on.
+// DOM elements by id imports
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+
+// fetch exchange rates and update DOM
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    console.log(currency_one, currency_two);
+}
+
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+calculate();
