62be990e921df1a65f19c3040c9ed349cbd55cc4
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-10 09:58:57 +0100

    collect DOM elements

--- dom-array-methods/scripts.js
+++ dom-array-methods/scripts.js
@@ -1,1 +1,9 @@
-ECHO is on.
+// collect DOM elements
+const addUserBtn = document.getElementById('add-user');
+const doubleMoneyBtn = document.getElementById('double');
+const showMillionaireBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+const main = document.getElementById('main');
+
+
