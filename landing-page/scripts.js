2cb04882218496813c6d73b1d8119ab3f0bda2ac
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-15 06:42:26 +0100

    added DOM element imports

--- landing-page/scripts.js
+++ landing-page/scripts.js
@@ -1,1 +1,5 @@
-ECHO is on.
+// Import DOM elements
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
