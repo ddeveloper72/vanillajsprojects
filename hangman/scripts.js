269098ac0d9b0eafa695d8674ec907cfcd0074ea
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-18 10:23:44 +0100

    add array of words and use math randome to select one

--- hangman/scripts.js
+++ hangman/scripts.js
@@ -1,7 +1,6 @@
 // Import DOM elements
 const wrongLettersEl = document.getElementById('wrong-letters');
 const wordEl = document.getElementById('word');
-const finalMessageEl = document.getElementById('final-message');
 const popupEl = document.getElementById('popup-container');
 const playBtnEl = document.getElementById('play-button');
 const finalMessageEl = document.getElementById('final-message');
@@ -10,3 +9,11 @@ const notificationEl = document.getElementById('notification-container');
 
 // DOM elements for svg drawing
 const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['hello', 'world', 'head', 'shoe', 'butterfly'];
+
+
+// select a random word from the words array
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+console.log(selectedWord);
