dec5c34388a7abe7a1821c4d6fccb3b254c54735
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-18 09:33:07 +0100

    add DOM elements for text

--- hangman/scripts.js
+++ hangman/scripts.js
@@ -1,1 +1,8 @@
-ECHO is on.
+// Import DOM elements
+const wrongLettersEl = document.getElementById('wrong-letters');
+const wordEl = document.getElementById('word');
+const finalMessageEl = document.getElementById('final-message');
+const popupEl = document.getElementById('popup-container');
+const playBtnEl = document.getElementById('play-button');
+const finalMessageEl = document.getElementById('final-message');
+const notificationEl = document.getElementById('notification-container');
