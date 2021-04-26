d013240c0942a6c6968e9a1fee9d6a887e9a15dc
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-26 22:23:17 +0100

    added DOM element imports

--- meal-finder/scripts.js
+++ meal-finder/scripts.js
@@ -1,1 +1,7 @@
-ECHO is on.
+// Import DOM elements
+const search = document.getElementById('search'),
+submit = document.getElementById('submit'),
+random = document.getElementById('random'),
+mealsEl = document.getElementById('meals'),
+resultsHeading = document.getElementById('results-heading'),
+single_mealEl = document.getElementById('single-meal')
