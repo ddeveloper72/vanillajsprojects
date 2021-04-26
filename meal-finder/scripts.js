314fc9369bb46ab7fb083e36d5244344991af44b
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-26 22:29:06 +0100

    build out function for searchMeal initiated by submit

--- meal-finder/scripts.js
+++ meal-finder/scripts.js
@@ -1,7 +1,19 @@
 // Import DOM elements
 const search = document.getElementById('search'),
-submit = document.getElementById('submit'),
-random = document.getElementById('random'),
-mealsEl = document.getElementById('meals'),
-resultsHeading = document.getElementById('results-heading'),
-single_mealEl = document.getElementById('single-meal')
+    submit = document.getElementById('submit'),
+    random = document.getElementById('random'),
+    mealsEl = document.getElementById('meals'),
+    resultsHeading = document.getElementById('results-heading'),
+    single_mealEl = document.getElementById('single-meal')
+
+
+// Search meal and fetch data from API
+function searchMeal(e) {
+    // this is a submit, so prevent default method of submitting data to a file
+    e.preventDefault();
+
+
+}
+
+// Event listeners
+submit.addEventListener('submit', searchMeal);
