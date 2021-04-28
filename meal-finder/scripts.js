b786047af65bbf6e4967092858e7cea5528da8fa
ddeveloper72 <duncan.niamh@gmail.com>
2021-04-28 22:27:11 +0100

    render the DOM elements

--- meal-finder/scripts.js
+++ meal-finder/scripts.js
@@ -75,9 +75,22 @@ function addMealToDOM(meal) {
     }
 
     // render the DOM elements
+    // map through each ingredient and render list item with ingredient as a string
     single_mealEl.innerHTML = `
     <div class="single-meal">
-    <h1>${meal.strMeal}</h1>
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+        <div class="single-meal-info">
+            ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+            ${meal.strAria ? `<P>${meal.strAria}</p>` : ''}
+        </div>
+        <div class="main">
+            <p>${meal.strInstructions}</p>
+            <h2>Ingredients</h2>
+            <ul>
+                ${ingredients.map(ing => `<li>${ing}<li>`).join('')}
+            </ul>
+        </div>
     </div>`;
 }
 
