bd3ecf2a2586c08e112fbb17face0f5a292aa664
ddeveloper72 <duncan.niamh@gmail.com>
2021-05-01 14:35:32 +0100

    added dom imports

--- expense-tracker/scripts.js
+++ expense-tracker/scripts.js
@@ -1,1 +1,8 @@
-ECHO is on.
+// add DOm imports
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const form = document.getElementById('form');
+const list = document.getElementById('list');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
