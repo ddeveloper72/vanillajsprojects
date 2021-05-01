2ef3852e600859efb62589392ff71992664e1e92
ddeveloper72 <duncan.niamh@gmail.com>
2021-05-01 14:45:00 +0100

    add construction data array

--- expense-tracker/scripts.js
+++ expense-tracker/scripts.js
@@ -6,3 +6,14 @@ const form = document.getElementById('form');
 const list = document.getElementById('list');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
+
+
+// construction array 
+const dummyTransaction = [
+    { id: 1, text: 'flowers', amount: -45},
+    { id: 2, text: 'Wages', amount: 800},
+    { id: 3, text: 'Aldi', amount: -40},
+    { id: 4, text: 'Diesel', amount: -60},
+    { id: 5, text: 'X-box', amount: -15},
+    { id: 6, text: 'PayPal-Alex', amount: 20}
+];
