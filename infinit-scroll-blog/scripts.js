cb51ef494520529b4e710ee24c92d305be6cd4b3
ddeveloper72 <duncan.niamh@gmail.com>
2021-05-17 14:21:54 +0100

    added getPosts function

--- infinit-scroll-blog/scripts.js
+++ infinit-scroll-blog/scripts.js
@@ -2,3 +2,15 @@
 const filter = document.getElementById('filter');
 const postContainer = document.getElementById('post-container');
 const loading = document.getElementsByClassName('.loader');
+
+
+let limit = 3;
+let page = 1;
+
+async function getPosts() {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_pages=${page}`);
+
+    const data = await res.json();
+    
+    return data;
+}
