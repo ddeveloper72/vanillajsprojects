e0b04d9a82f00069b078202a6d03f94e919fe8b4
ddeveloper72 <duncan.niamh@gmail.com>
2021-05-17 14:09:25 +0100

    added DOM imports

--- infinit-scroll-blog/scripts.js
+++ infinit-scroll-blog/scripts.js
@@ -1,1 +1,4 @@
-ECHO is on.
+// DOM ID imports
+const filter = document.getElementById('filter');
+const postContainer = document.getElementById('post-container');
+const loading = document.getElementsByClassName('.loader');
