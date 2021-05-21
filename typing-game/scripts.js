7b179ed7bb4d5625160cad3216bfa034fb8032f7
ddeveloper72 <duncan.niamh@gmail.com>
2021-05-21 14:13:56 +0100

    added DOM imports

--- typing-game/scripts.js
+++ typing-game/scripts.js
@@ -1,1 +1,10 @@
-ECHO is on.
+// DOM Imports
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings-btn');
+const settingsForm = document.getElementById('settings-form');
+const difficultySelect = document.getElementById('difficulty');
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const timeEl = document.getElementById('time');
+const scoreEl = document.getElementById('score');
+const endgameEl = document.getElementById('end-game-container');
