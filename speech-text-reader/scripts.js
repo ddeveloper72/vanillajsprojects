b212fc258cfb8b8ebdaf618949c7d8298e28a92b
ddeveloper72 <duncan.niamh@gmail.com>
2021-05-24 17:35:50 +0100

    added DOM element imports

--- speech-text-reader/scripts.js
+++ speech-text-reader/scripts.js
@@ -1,1 +1,7 @@
-ECHO is on.
+// Import DOm selectors
+const main = document.querySelector('main');
+const toggleBtn = document.getElementById('toggle');
+const textBox = document.getElementById('toggle-box');
+const closeBtn = document.getElementById('close');
+const voicesSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
