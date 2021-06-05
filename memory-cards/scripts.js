b2bd59d3aca3d3e4e2ff6d172a8cd85f5a87dbef
ddeveloper72 <duncan.niamh@gmail.com>
2021-06-05 18:37:05 +0100

    added DOM slector imports

--- memory-cards/scripts.js
+++ memory-cards/scripts.js
@@ -1,1 +1,12 @@
-ECHO is on.
+// Dome selector imports
+const clearBtn = document.getElementById('clear');
+const showBtn = document.getElementById('show');
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const currentEl = document.getElementById('current');
+const nextBtn = document.getElementById('next');
+const addContainer = document.getElementById('add-container');
+const hideBtn = document.getElementById('hide');
+const questionEl = document.getElementById('question');
+const answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
