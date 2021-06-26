e4fc51c2fc5096bdeef0d10078a4146f922d0766
ddeveloper72 <duncan.niamh@gmail.com>
2021-06-26 12:19:09 +0100

    add event listener for search term

--- lyrics-search/scripts.js
+++ lyrics-search/scripts.js
@@ -7,3 +7,12 @@ const more = document.getElementById('more');
 
 // Import API
 const apiURL = 'https://api.lyrics.ovh';
+
+// Event listeners
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+
+    console.log(searchTerm);
+});
