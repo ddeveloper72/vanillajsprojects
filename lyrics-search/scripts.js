e5ca7dd3f7ba04bc73298de8d76a718d63423df7
ddeveloper72 <duncan.niamh@gmail.com>
2021-06-26 12:14:35 +0100

    added dom element imports

--- lyrics-search/scripts.js
+++ lyrics-search/scripts.js
@@ -1,5 +1,8 @@
 // Import DOM elements
-
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
 
 
 // Import API
