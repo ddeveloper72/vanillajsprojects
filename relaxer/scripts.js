a65016e77ba1393f45027d91462d552ed6a85f9a
ddeveloper72 <duncan.niamh@gmail.com>
2021-09-01 22:12:25 +0100

    add constants for the breathing cycle

--- relaxer/scripts.js
+++ relaxer/scripts.js
@@ -1,3 +1,11 @@
 // Import doc elements
 const container = document.getElementById('container');
 const text = document.getElementById('text');
+
+
+// 7.5 seconds to breathe in then breath out
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+console.log(breatheTime, holdTime);
