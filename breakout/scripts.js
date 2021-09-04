582b0eb78b05333fee29959b528ba3cb45a03f03
ddeveloper72 <duncan.niamh@gmail.com>
2021-09-04 14:56:08 +0100

    added document elements by Id

--- breakout/scripts.js
+++ breakout/scripts.js
@@ -1,1 +1,6 @@
-ECHO is on.
+const rulesBtn = document.getElementById('rules-btn');
+const rules = document.getElementById('rules');
+const closeBtn = document.getElementById('close-btn');
+const canvas = document.getElementById('canvas');
+
+
