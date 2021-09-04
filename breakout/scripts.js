d5f2296eca4104b91515de8b06bab512b5a5e539
ddeveloper72 <duncan.niamh@gmail.com>
2021-09-04 14:58:44 +0100

    add show class to rules btn

--- breakout/scripts.js
+++ breakout/scripts.js
@@ -4,3 +4,6 @@ const closeBtn = document.getElementById('close-btn');
 const canvas = document.getElementById('canvas');
 
 
+// Rules and close Event listeners
+rulesBtn.addEventListener('click', () => 
+rules.classList.add('show'));
