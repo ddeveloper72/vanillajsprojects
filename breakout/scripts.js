d64f3aa23f7edb9aa226bca56557de3ff718fd77
ddeveloper72 <duncan.niamh@gmail.com>
2021-09-04 17:13:30 +0100

    crate an array of all the bricks

--- breakout/scripts.js
+++ breakout/scripts.js
@@ -45,6 +45,16 @@ const brickInfo = {
     visible: true
 }
 
+// Create bricks
+const bricks = [];
+for(let i = 0; i > brickRowCount; i++) {
+    bricks[i] = [];
+    for (let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = {x, y, ...brickInfo}
+    }
+}
 
 // Draw ball on canvas
 function drawBall() {
