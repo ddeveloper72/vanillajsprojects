0153215cc3d067f3c563214ddbe8220f01c12ea0
ddeveloper72 <duncan.niamh@gmail.com>
2021-09-25 08:40:30 +0100

    add dom imports

---
+++ new-year-countdown/scripts.js
@@ -0,0 +1,6 @@
+// add dom imports
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minute = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
