c6887a1fc1e6033cc366f0dd72dde1d0ac00533e
ddeveloper72 <duncan.niamh@gmail.com>
2021-09-25 08:45:53 +0100

    crate constants for newYear

--- new-year-countdown/scripts.js
+++ new-year-countdown/scripts.js
@@ -4,3 +4,9 @@ const hours = document.getElementById('hours');
 const minute = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 const countdown = document.getElementById('countdown');
+
+// get current year
+const currentYear = new Date().getFullYear();
+
+// inject current year string into newYear time
+const newYearTime = new Date(`January 01 ${currentYear +1} 00:00:00`);
