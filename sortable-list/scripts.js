e95573767982c939ae58da06452012327d08fd71
ddeveloper72 <duncan.niamh@gmail.com>
2021-10-02 13:44:52 +0100

    create a richlist array

--- sortable-list/scripts.js
+++ sortable-list/scripts.js
@@ -1,1 +1,16 @@
-ECHO is on.
+// import DOM elements
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const richList = [
+    'Hilary, Alannah and Galen Jr Weston and family',
+    'Patrick and John Collison',
+    'John Dorrance',
+    'Larry, Kitty, Laurence and Mark Goodman',
+    'Dermot Desmond',
+    'Denis O’Brien',
+    'Lady Ballyedmond and family',
+    'The Dunne and Heffernan families',
+    'John Magnier and family',
+    'Deirdre and Mark Lyons'
+];
