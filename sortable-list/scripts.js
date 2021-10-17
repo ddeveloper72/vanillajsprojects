700770c913cf74a4cdc04b8d27671b008d78742b
ddeveloper72 <duncan.niamh@gmail.com>
2021-10-17 08:57:23 +0100

    refactored richlist and into an array of objects

--- sortable-list/scripts.js
+++ sortable-list/scripts.js
@@ -3,17 +3,58 @@ const draggable_list = document.getElementById('draggable-list');
 const check = document.getElementById('check');
 
 const richList = [
-    'Hilary, Alannah and Galen Jr Weston and family',
-    'Patrick and John Collison',
-    'John Dorrance',
-    'Larry, Kitty, Laurence and Mark Goodman',
-    'Dermot Desmond',
-    'Denis O’Brien',
-    'Lady Ballyedmond and family',
-    'The Dunne and Heffernan families',
-    'John Magnier and family',
-    'Deirdre and Mark Lyons'
+    {
+        name: "John Dorrance III",
+        networth: 2.8,
+        source: "Campbell Soup Company"
+    },
+    {
+        name: "John Magnier",
+        networth: 2.5,
+        source: "Coolmore Stud"
+    },
+    {
+        name: "J. P. McManus",
+        networth: 2.3,
+        source: "Foreign exchange market"
+    },
+    {
+        name: "Dermot Desmond",
+        networth: 2.1,
+        source: "Celtic F.C."
+    },
+    {
+        name: "Patrick Collis",
+        networth: 1.9,
+        source: "Stripe (company)"
+    },
+    {
+        name: "John Collison",
+        networth: 1.9,
+        source: "Stripe (company)"
+    },
+    {
+        name: "Martin Naughto",
+        networth: 1.6,
+        source: "GlenDimplex"
+    },
+    {
+        name: "Eugene Murtagh",
+        networth: 1.4,
+        source: "Kingspan Group"
+    },
+    {
+        name: "Luke and Brianomer",
+        networth: 1.6,
+        source: "Comer Group"
+    },
+    {
+        name: "David McMurtry",
+        networth: 1.2,
+        source: "Renishaw plc"
+    }
 ];
+
 
 //  Store list items
 const listItems = [];
@@ -44,7 +85,10 @@ function createList() {
             listItem.innerHTML = `
                 <span class="number">${index + 1}</span>
                 <div class="draggable" draggable="true">
-                  <p class="person-name">${unit}</p>
+                  <p class="person-name">${unit.name}</p>
+                  <p class="person-networth" id="networth">${unit.networth} billion</p>
+                  <p class="person-source">${unit.source}</p>
+                  
                   <i class="fas fa-grip-lines"></i>
                 </div>
             `;
@@ -52,7 +96,10 @@ function createList() {
             listItems.push(listItem);
 
             draggable_list.appendChild(listItem);
+
+           
         });
+
 
     addEventListeners();
 }
@@ -103,18 +150,26 @@ function swapItems(fromIndex, toIndex) {
 // Check the li order
 function checkOrder() {
     listItems.forEach((listItem, index) => {
-        const unitName = listItem.querySelector('.draggable')
+        // console.log(richList[index].name);
+
+
+        const unitName = listItem.querySelector('.draggable .person-name')
             .innerText.trim();
 
-        if (unitName != richList[index]) {
+        if (unitName != richList[index].name) {
             listItem.classList.add('wrong');
             listItem.classList.remove('right');
         } else {
             listItem.classList.remove('wrong');
             listItem.classList.add('right');
         }
+
+
+
     });
+
 }
+
 
 function addEventListeners() {
     const draggables = document.querySelectorAll('.draggable');
