5df0e535dc986a6efb85fb92fe740147f9154102
ddeveloper72 <duncan.niamh@gmail.com>
2021-10-23 08:09:10 +0100

    generated random number

--- speak-a-number/scripts.js
+++ speak-a-number/scripts.js
@@ -5,7 +5,8 @@ const msgEl = document.getElementById('msg');
 // call a function to get a random number
 const randomNum = getRandomNumber();
 
+console.log('Number is: ' + randomNum);
 
 function getRandomNumber() {
-    
+    return Math.floor(Math.random() * 100) + 1
 }
