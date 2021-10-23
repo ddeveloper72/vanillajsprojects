64aaf20f2e591090a57e15adf87eae10f67ec5ad
ddeveloper72 <duncan.niamh@gmail.com>
2021-10-23 08:14:41 +0100

    setup speech recognition api

--- speak-a-number/scripts.js
+++ speak-a-number/scripts.js
@@ -7,6 +7,13 @@ const randomNum = getRandomNumber();
 
 console.log('Number is: ' + randomNum);
 
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition game
+recognition.start();
+
 function getRandomNumber() {
     return Math.floor(Math.random() * 100) + 1
 }
