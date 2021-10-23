001fa13e016956685bb18ff09be458ac97968dc2
ddeveloper72 <duncan.niamh@gmail.com>
2021-10-23 08:05:50 +0100

    setup js boiler

--- speak-a-number/scripts.js
+++ speak-a-number/scripts.js
@@ -1,1 +1,11 @@
-ECHO is on.
+// Import DOm element
+const msgEl = document.getElementById('msg');
+
+
+// call a function to get a random number
+const randomNum = getRandomNumber();
+
+
+function getRandomNumber() {
+    
+}
